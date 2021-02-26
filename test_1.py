75a41c1ec0a2e0da64d595bb3cdb5ae6e5350df0
99003723 <vipul.sakunia@ltts.com>
2021-02-26 09:07:02 +0530

    commit it

--- test_1.py
+++ test_1.py
@@ -5,16 +5,17 @@ software=[]
 lines=0
 
 pattern=re.compile("Software",re.I|re.M)
-with open('input.txt','rt') as file_info:
+
+with open(r'D:\Ltts training\Mini_project\input.txt','rt') as file_info:
 
     for file_line in file_info:
         lines +=1
         if pattern.search(file_line)!=None:
             software.append((lines,file_line.rstrip('\n')))
-        #print(lines)
 
-        for ans in software:
-            count+=1
-            with open("software.txt",'a') as file_ans:
-                file_ans.writelines(str(count)+':')
-                file_ans.writelines(ans[1]+'\n')
+    for ans in software:
+        count+=1
+        with open("software.txt",'a') as file_ans:
+            file_ans.writelines(str(count)+':')
+            file_ans.writelines(ans[1]+'\n')
+print(count)
