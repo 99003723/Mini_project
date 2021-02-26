6a20a8676f8838047fbfd8691ad6e45a9541e88b
99003723 <vipul.sakunia@ltts.com>
2021-02-26 09:10:58 +0530

    code part

--- word_finder.py
+++ word_finder.py
@@ -1,16 +1,30 @@
 import re
 
 
-count=0
-findword=input("Enter the word:\n")
-pattern=re.compile(findword,re.I|re.M)
-with open (r'D:\Ltts training\Mini_project\input.txt', 'rt') as hand:
-    lines=hand.readlines()
-    for line in lines:
-        wordfinder= re.findall(pattern,line)
-        
-        for word in wordfinder:
-            print(word)
-            count+=1
+string = open('input.txt').read()
+new_string = re.sub(r'[^a-zA-Z\n]', ' ', string)
+text_file = open("sample.txt", "w")
+n = text_file.write(new_string)
+text_file.close()
 
-print(count)
+
+count = 0
+findword = input("Enter the word:\n")
+pattern = re.compile(findword, re.I | re.M)
+hand = open(r'sample.txt')
+handle = hand.read()
+wordfinder = re.findall(pattern, handle)
+file_list = handle.split()
+
+create_file = findword + '.txt'
+file_gen = open(create_file, 'w+')
+for i in range(len(file_list)):
+    wordfound = re.match(findword, file_list[i], re.I | re.M)
+    if wordfound:
+        count += 1
+
+        sent = (file_list[i-1] + ' ' + file_list[i] + ' ' + file_list[i+1])
+        file_gen.write(str(sent) + '\n')
+        file_gen.write(str(count) + ' :')
+
+file_gen.write('word repeated:' + str(len(wordfinder)) + '\n')  #pep8 done w/o comments
